b5eb3c20d4b1b478341bc13c6f8b796f083dff2d
CubeJunkie <marcosalberto.maldonado@gmail.com>
2026-09-14 13:50:34 -0300

    Se agrega puntuación a la función de saludo al usuario

--- 06_Funciones/auxiliar/saludos.py
+++ 06_Funciones/auxiliar/saludos.py
@@ -4,7 +4,7 @@ def hola_mundo():
 
 def saludar_usuario(nombre):
     '''Saluda al usuario por su nombre, dado el nombre'''
-    print(f'Hola {nombre}')
+    print(f'Hola, {nombre}.')
 
 def informacion_persona(nombre, apellido, edad, residencia):
     '''Muestra en pantalla una frase de presentación dados los datos de nombre, apellido, edad y lugar de residencia'''
